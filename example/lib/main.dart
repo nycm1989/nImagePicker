625f0f7d033dba6ef1ccda4e17b53571d990fbf7
moinkhan780 <88693191+moinkhan780@users.noreply.github.com>
2024-08-22 10:05:09 +0400

    Optimize code structure

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -13,146 +13,154 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  List<ImageController> imageControllers = [
-    ImageController(),
-    ImageController(),
-    ImageController(),
-    ImageController(),
-  ];
+  final List<ImageController> imageControllers = List.generate(4, (_) => ImageController());
 
   @override
   void initState() {
     super.initState();
-    for(ImageController controller in imageControllers){
+    for (var controller in imageControllers) {
       controller.addListener(() => setState(() {}));
     }
   }
 
   @override
   void dispose() {
-    super.dispose();
-    for(ImageController controller in imageControllers){
-      controller
-      ..removeListener((){})
-      ..dispose();
+    for (var controller in imageControllers) {
+      controller.removeListener(() => setState(() {}));
+      controller.dispose();
     }
+    super.dispose();
   }
 
-  // static const String image = 'https://w.wallhaven.cc/full/49/wallhsaven-49d5y8.jpg';
-  static const String image = 'http://192.168.4.233:8000/media/202408/IMAGES/IMAGE_f0676afb-808f-4cf4-b26a-2a483e531623.jpg';
+  static const String image = 'https://w.wallhaven.cc/full/49/wallhaven-49d5y8.jpg';
 
   @override
-  Widget build(BuildContext context) =>
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Pin Board Example',
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: SingleChildScrollView(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  // ImagePicker Rectangle
+                  ImagePicker(
+                    controller: imageControllers[0],
+                    width: 200,
+                    height: 150,
+                    onLoadingImage: image,
+                    backgroundColor: const Color(0xFFededed).withOpacity(0.8),
+                    filterOpacity: 0.2,
+                    borderRadius: BorderRadius.circular(20),
+                    fit: BoxFit.cover,
+                    border: Border.all(color: Colors.grey, width: 1),
+                    shadow: const BoxShadow(color: Colors.black, blurRadius: 5, blurStyle: BlurStyle.outer),
+                    viewerBlur: true,
+                    viewerBlurSigma: 10,
+                    previewBlur: true,
+                    previewBlurSigma: 1,
+                  ),
+                  const SizedBox(height: 16),
 
-    MaterialApp(
-      debugShowCheckedModeBanner: true,
-      title : 'Pin Board Example',
-      home  :
-      Scaffold(
-        body:
-        Column(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ImagePicker(
-                  controller        : imageControllers[0],
-                  width             : 200,
-                  height            : 150,
-                  onLoadingImage    : image,
-                  bankgroundColor   : const Color(0xFFededed).withOpacity(0.8),
-                  filterOpacity     : 0.2,
-                  borderRadius      : BorderRadius.circular(50),
-                  fit               : BoxFit.cover,
-                  border            : Border.all(color: Colors.grey, width: 1),
-                  shadow            : const BoxShadow(color: Colors.black, blurRadius: 5, blurStyle: BlurStyle.outer),
-                  viewerBlur        : true,
-                  viewerBlurSigma   : 10,
-                  previewBlur       : true,
-                  previewBlurSigma  : 1,
-                ),
-                ImagePicker.circle(
-                  tag               : "TAGFORTESTING01",
-                  controller        : imageControllers[1],
-                  onLoadingImage    : image,
-                  bankgroundColor   : const Color(0xFFededed).withOpacity(0.8),
-                  dimension         : 200,
-                  filterOpacity     : 0.2,
-                  fit               : BoxFit.cover,
-                  border            : Border.all(color: Colors.grey, width: 1),
-                  shadow            : const BoxShadow(color: Colors.black, blurRadius: 5, blurStyle: BlurStyle.outer),
-                  viewerBlur        : true,
-                  viewerBlurSigma   : 10,
-                  previewBlur       : true,
-                  previewBlurSigma  : 1,
-                  closeColor        : Colors.grey,
-                  maxSize           : 500
-                ),
-                ImagePicker.square(
-                  controller        : imageControllers[2],
-                  bankgroundColor   : const Color(0xFFededed).withOpacity(0.8),
-                  dimension         : 200,
-                  filterOpacity     : 0.2,
-                  borderRadius      : BorderRadius.circular(50),
-                  fit               : BoxFit.cover,
-                  border            : Border.all(color: Colors.grey, width: 1),
-                  shadow            : const BoxShadow(color: Colors.black, blurRadius: 5, blurStyle: BlurStyle.outer),
-                  viewerBlur        : true,
-                  viewerBlurSigma   : 10,
-                  previewBlur       : true,
-                  previewBlurSigma  : 1,
-                ),
-                SizedBox(
-                  width  : 100,
-                  height : 200,
-                  child  :
-                  ImagePicker.expand(
-                    tag               : "TAGFORTESTING02",
-                    controller        : imageControllers[3],
-                    bankgroundColor   : const Color(0xFFededed).withOpacity(0.8),
-                    onLoadingImage    : image,
-                    filterOpacity     : 0.2,
-                    borderRadius      : BorderRadius.circular(50),
-                    fit               : BoxFit.cover,
-                    border            : Border.all(color: Colors.grey, width: 1),
-                    shadow            : const BoxShadow(color: Colors.black, blurRadius: 5, blurStyle: BlurStyle.outer),
-                    viewerBlur        : true,
-                    viewerBlurSigma   : 10,
-                    previewBlur       : true,
-                    previewBlurSigma  : 1,
+                  // ImagePicker Circle
+                  ImagePicker.circle(
+                    tag: "TAGFORTESTING01",
+                    controller: imageControllers[1],
+                    onLoadingImage: image,
+                    backgroundColor: const Color(0xFFededed).withOpacity(0.8),
+                    dimension: 200,
+                    filterOpacity: 0.2,
+                    fit: BoxFit.cover,
+                    border: Border.all(color: Colors.grey, width: 1),
+                    shadow: const BoxShadow(color: Colors.black, blurRadius: 5, blurStyle: BlurStyle.outer),
+                    viewerBlur: true,
+                    viewerBlurSigma: 10,
+                    previewBlur: true,
+                    previewBlurSigma: 1,
+                    closeColor: Colors.grey,
+                    maxSize: 500,
                   ),
-                )
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                ImageViewer(
-                  image   : image,
-                  width   : 200,
-                  height  : 100,
-                ),
-                ImageViewer.square(
-                  image     : image,
-                  dimension : 200,
-                ),
-                ImageViewer.circle(
-                  image     : image,
-                  dimension : 200,
-                ),
-                SizedBox(
-                  width  : 100,
-                  height : 200,
-                  child  :
-                  ImageViewer.expand(
+                  const SizedBox(height: 16),
+
+                  // ImagePicker Square
+                  ImagePicker.square(
+                    controller: imageControllers[2],
+                    backgroundColor: const Color(0xFFededed).withOpacity(0.8),
+                    dimension: 200,
+                    filterOpacity: 0.2,
+                    borderRadius: BorderRadius.circular(20),
+                    fit: BoxFit.cover,
+                    border: Border.all(color: Colors.grey, width: 1),
+                    shadow: const BoxShadow(color: Colors.black, blurRadius: 5, blurStyle: BlurStyle.outer),
+                    viewerBlur: true,
+                    viewerBlurSigma: 10,
+                    previewBlur: true,
+                    previewBlurSigma: 1,
+                  ),
+                  const SizedBox(height: 16),
+
+                  // ImagePicker Expand
+                  SizedBox(
+                    width: 100,
+                    height: 200,
+                    child: ImagePicker.expand(
+                      tag: "TAGFORTESTING02",
+                      controller: imageControllers[3],
+                      backgroundColor: const Color(0xFFededed).withOpacity(0.8),
+                      onLoadingImage: image,
+                      filterOpacity: 0.2,
+                      borderRadius: BorderRadius.circular(20),
+                      fit: BoxFit.cover,
+                      border: Border.all(color: Colors.grey, width: 1),
+                      shadow: const BoxShadow(color: Colors.black, blurRadius: 5, blurStyle: BlurStyle.outer),
+                      viewerBlur: true,
+                      viewerBlurSigma: 10,
+                      previewBlur: true,
+                      previewBlurSigma: 1,
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+
+                  // ImageViewer Rectangle
+                  ImageViewer(
                     image: image,
+                    width: 200,
+                    height: 100,
                   ),
-                )
-              ],
+                  const SizedBox(height: 16),
+
+                  // ImageViewer Square
+                  ImageViewer.square(
+                    image: image,
+                    dimension: 200,
+                  ),
+                  const SizedBox(height: 16),
+
+                  // ImageViewer Circle
+                  ImageViewer.circle(
+                    image: image,
+                    dimension: 200,
+                  ),
+                  const SizedBox(height: 16),
+
+                  // ImageViewer Expand
+                  SizedBox(
+                    width: 100,
+                    height: 200,
+                    child: ImageViewer.expand(
+                      image: image,
+                    ),
+                  ),
+                ],
+              ),
             ),
-          ],
+          ),
         ),
       ),
     );
+  }
 }
